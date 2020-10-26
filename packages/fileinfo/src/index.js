99e9229c7a8c207330a31e571b9973961d0e8015
babbage <601728820@qq.com>
2020-10-26 11:55:40 +0800

    feat: fileinfo 的基本目标确定

---
+++ packages/fileinfo/src/index.js
@@ -0,0 +1,47 @@
+class FileInfo {
+  construct(filePath) {}
+  /**
+   * Gets the base name of the file
+   */
+  getBasename() {}
+  /**
+   * Gets the file extension
+   */
+  getExtension() {}
+  /**
+   *  Gets the filename
+   */
+  getFilename() {}
+  /**
+   *  Gets the path without filename
+   */
+  getPath() {}
+  /**
+   * Gets file size
+   */
+  getSize() {}
+  /**
+   * Gets file type
+   */
+  getType() {}
+  /**
+   *  Tells if the file is a directory
+   */
+  isDir() {}
+  /**
+   * Tells if the object references a regular file
+   */
+  isFile() {}
+  /**
+   * Tells if the file is a link
+   */
+  isLink() {}
+  /**
+   *  Tells if file is readable
+   */
+  isReadable() {}
+  /**
+   * Tells if the entry is writable
+   */
+  isWritable() {}
+}
