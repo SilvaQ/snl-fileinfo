f05c79d5dcc2f9ce3d4ee3c571523cdb734bc8da
babbage <601728820@qq.com>
2020-10-29 15:39:54 +0800

    feat: 补充了finder的测试用例,precommit的hooks补充了test

--- packages/fileinfo/__tests__/index.test.ts
+++ packages/fileinfo/__tests__/index.test.ts
@@ -2,7 +2,20 @@ import {Finder} from  '../src/finder'
 import {join} from 'path'
 const finder =new Finder(join(__dirname,"tmp"))
 
-test('test all files', () => {
+test('test finder allFiles', () => {
   const allfiles=finder.allFiles();
   expect(allfiles.length).toEqual(6);
+  expect(allfiles.map(fileinfo=>fileinfo.getFilename())).toEqual(["a", "aa.md", "b", "bb.md", "c", "cc.md"]);
+});
+
+test('test finder directories', () => {
+  const allfiles=finder.directories();
+  expect(allfiles.length).toEqual(3);
+  expect(allfiles.map(fileinfo=>fileinfo.getFilename())).toEqual(["a",  "b",  "c", ]);
+});
+
+test('test finder files', () => {
+  const allfiles=finder.files();
+  expect(allfiles.length).toEqual(3);
+  expect(allfiles.map(fileinfo=>fileinfo.getFilename())).toEqual(["aa.md", "bb.md", "cc.md"]);
 });
